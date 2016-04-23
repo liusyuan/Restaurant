80f4109803d273c8e23ccc3da2e4b2e3d55f651a
liusyuan <lsy561061800@163.com>
2016-04-23 15:50:41 +0800

    Restaurant-Maven

--- src/main/java/com/netease/NoodlesServlet.java
+++ src/main/java/com/netease/NoodlesServlet.java
@@ -3,16 +3,20 @@ package com.netease;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class NoodlesServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException {
+    	Logger logger=Logger.getLogger(HttpServletRequest.class.getName());	
+    	Date date=new Date();
         PrintWriter writer = response.getWriter();
         String vegetable = request.getParameter("vegetable");
         if (vegetable == null) {
@@ -22,5 +26,6 @@ public class NoodlesServlet extends HttpServlet {
         writer.println("<html><body>");
         writer.println("<h1> Noodles with " + vegetable + "</h1>"); 
         writer.println("</body></html>");
+        logger.info(date.getTime()+vegetable);
     }
 }
